b3f91e82fc1fe16f06feb909b45f0fe584a52f8a
Nattawat Ngowthammatas <nattawat.ngowthammatas28@thehutgroup.com>
2024-01-23 10:39:44 +0000

    created FinalBoard int[][] class

--- src/main/java/com/thg/accelerator23/connectn/ai/connectbruh/Node.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/connectbruh/Node.java
@@ -8,11 +8,11 @@ public class Node {
     private Node[] children;
     private int visits;
     private double winCount;
-    private final Board board;
+    private final FinalBoard finalBoard;
 
-    public Node(Node parent, Board board) {
+    public Node(Node parent, FinalBoard finalBoard) {
         this.parent = parent;
-        this.board = board;
+        this.finalBoard = finalBoard;
     }
 
     public int incrementVisits() {
